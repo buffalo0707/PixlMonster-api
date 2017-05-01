e4840f0220287933a7519c325510c4fe2741eb1e
buffalo0707 <alan.henderson@libertymutual.com>
2017-05-01 10:06:10 -0400

    Adds default value for mood in monsters
    mood now defaults to 'happy'

--- db/migrate/20170501140338_change_mood_default_in_monsters.rb
+++ db/migrate/20170501140338_change_mood_default_in_monsters.rb
@@ -1,4 +1,4 @@
-class ChangeMoodTypeInMonstersTable < ActiveRecord::Migration[5.0]
+class ChangeMoodDefaultInMonsters < ActiveRecord::Migration[5.0]
   def change
     change_column :monsters, :mood, :string, default: 'happy'
   end
